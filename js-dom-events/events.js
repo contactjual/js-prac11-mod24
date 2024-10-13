62cd200fe9affac3f74e422cec82c1eed88c9a01
contactjual <contactjual@gmail.com>
2024-10-13 14:28:52 +0600

    commiteCounting

--- js-dom-events/events.js
+++ js-dom-events/events.js
@@ -1,1 +1,25 @@
-console.log('this is js file')
+
+// process no2
+
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+
+
+
+
+// process no3
+
+const makeBlueButton = document.getElementById('make-blue')
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+
+// or 
+
+// makeBlueButton.onclick = function makeBlue() {
+//     document.body.style.backgroundColor = 'blue';
+
+}
